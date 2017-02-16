d0283812dfd7f90d14224f46159aefeaf11f6ab1
Jochen Klein <jochen.klein@cern.ch>
2017-02-16 15:08:25 +0100

    Add AMPT

---
+++ ampt.sh
@@ -0,0 +1,33 @@
+package: AMPT
+version: "alice/v1.26t7-v2.26t7"
+source: https://github.com/alisw/ampt
+requires:
+ - "GCC-Toolchain:(?!osx)"
+---
+#!/bin/bash -e
+
+rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ ./
+make -j${JOBS}
+install -d $INSTALLROOT/bin
+install -t $INSTALLROOT/bin ampt
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+cat > "$MODULEFILE" <<EoF
+#%Module1.0
+proc ModulesHelp { } {
+  global version
+  puts stderr "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+}
+set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
+module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
+# Dependencies
+module load BASE/1.0
+# Our environment
+setenv AMPT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
+prepend-path PATH \$::env(AMPT_ROOT)/bin
+#prepend-path LD_LIBRARY_PATH \$::env(AMPT_ROOT)/lib
+#$([[ ${ARCHITECTURE:0:3} == osx ]] && echo "prepend-path DYLD_LIBRARY_PATH \$::env(AMPT_ROOT)/lib")
+EoF
