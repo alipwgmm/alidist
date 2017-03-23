a7307e02dc1280bf426557e3e701b05cb37bc6a5
rbertens <rbertens@cern.ch>
2017-03-23 11:06:27 +0100

    Add AMPT-to-HepMC parser

--- ampt.sh
+++ ampt.sh
@@ -11,6 +11,7 @@ rsync -a --exclude='**/.git' --delete --delete-excluded $SOURCEDIR/ ./
 make -j${JOBS}
 install -d $INSTALLROOT/bin
 install -t $INSTALLROOT/bin ampt
+install -t $INSTALLROOT/bin parser
 
 # Modulefile
 MODULEDIR="$INSTALLROOT/etc/modulefiles"
@@ -25,7 +26,7 @@ proc ModulesHelp { } {
 set version $PKGVERSION-@@PKGREVISION@$PKGHASH@@
 module-whatis "ALICE Modulefile for $PKGNAME $PKGVERSION-@@PKGREVISION@$PKGHASH@@"
 # Dependencies
-module load BASE/1.0 ${HEPMC_VERSION:+HEPMC/$HEPMC_VERSION-$HEPMC_REVISION}
+module load BASE/1.0 ${HEPMC_VERSION:+HepMC/$HEPMC_VERSION-$HEPMC_REVISION}
 # Our environment
 setenv AMPT_ROOT \$::env(BASEDIR)/$PKGNAME/\$version
 prepend-path PATH \$::env(AMPT_ROOT)/bin
